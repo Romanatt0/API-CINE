38440a868e1e1946c9e0d8302089cdbc44b89548
Victor Cesar Romanatto - Citadel <60002369@tupy.com.br>
2026-02-24 12:58:22 -0300

    Criado schema UserLogin

--- schemas.py
+++ schemas.py
@@ -5,3 +5,8 @@ class UserCreate(BaseModel):
     email: str
     name: str
     password: str
+
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
--- user_routes.py
+++ user_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from schemas import UserCreate
+from schemas import UserCreate, UserLogin
 from dependencies import get_session
 from models import User
 from main import bcrypt_hash, SECRET_KEY, HASH
@@ -20,10 +20,10 @@ def create_user(user_create: UserCreate, session=Depends(get_session)):
     raise HTTPException(status_code=201, detail="User created successfully")
 
 @user_router.post("/login")
-def login_user(user_create: UserCreate, session=Depends(get_session)):
-    user = session.query(User).filter(User.email == user_create.email).first()
+def login_user(user_login: UserLogin, session=Depends(get_session)):
+    user = session.query(User).filter(User.email == user_login.email).first()
 
-    if not user or not bcrypt_hash.verify(user_create.password, user.password):
+    if not user or not bcrypt_hash.verify(user_login.password, user.password):
         raise HTTPException(status_code=400, detail="Invalid email or password")
     
     # Geração do token JWT
@@ -31,6 +31,7 @@ def login_user(user_create: UserCreate, session=Depends(get_session)):
         "sub": user.username,
         "exp": datetime.utcnow() + timedelta(hours=1)  
     }
+
     token = jwt.encode(payload, SECRET_KEY, algorithm=HASH)
     
     return {"access_token": token, "token_type": "bearer"}
