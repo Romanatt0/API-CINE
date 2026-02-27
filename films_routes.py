d207dbf2ac57a6aded75694b499696c38f0a92ec
Victor Cesar Romanatto - Citadel <60002369@tupy.com.br>
2026-02-27 13:00:56 -0300

    endpoint

--- films_routes.py
+++ films_routes.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from schemas import FilmCreate
 from dependencies import get_session
+from models import Film
 
 film_router = APIRouter(prefix="/films", tags=["films"])
 
 
 film_router.get()
 def find_all_films(films: list[FilmCreate], session=Depends(get_session)):
+
+    films: list[Film] = session.query(Film)
